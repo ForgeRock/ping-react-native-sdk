e23cdb2b1f26bac7492358ebf1f51a63bcd4b67d
Gaurav Gill <gauravgill@pingidentity.com>
2025-11-14 16:11:37 -0500

    chore: mono repo tasks

--- PingSampleApp/metro.config.js
+++ PingSampleApp/metro.config.js
@@ -6,15 +6,17 @@ const oidcPackage = path.resolve(__dirname, '../packages/oidc');
 const journeyPackage = path.resolve(__dirname, '../packages/journey');
 const storagePackage = path.resolve(__dirname, '../packages/storage');
 
+const workspaceRoot = path.resolve(__dirname, '..');
+
 const config = {
-  watchFolders: [ oidcPackage, journeyPackage, storagePackage],
+  watchFolders: [ oidcPackage, journeyPackage, storagePackage, workspaceRoot],
   resolver: {
     nodeModulesPaths: [
       path.resolve(__dirname, 'node_modules'),
-    //  path.resolve(storagePackage, 'node_modules'),
       path.resolve(oidcPackage, 'node_modules'),
       path.resolve(journeyPackage, 'node_modules'),
-      path.resolve(storagePackage, 'node_modules')
+      path.resolve(storagePackage, 'node_modules'),
+      path.resolve(workspaceRoot, 'node_modules')
     ],
     extraNodeModules: {
       '@react-native-pingidentity/oidc': oidcPackage,
